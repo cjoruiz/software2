85127b047a19279c6d5031814f1688a77461f815
cjoruiz <cjoruiz@hotmail.es>
2025-08-16 12:13:18 -0500

    se ñade la interfaz grafica rn lr paquete presentatiom, se añaden dependencias layout y sqlite

--- src/main/java/co/unicauca/solid/main/Main.java
+++ src/main/java/co/unicauca/solid/main/Main.java
@@ -4,13 +4,17 @@
 
 package co.unicauca.solid.main;
 
+import co.unicauca.presentation.GUIDashboard;
+
 /**
  *
  * @author crist
  */
-public class Solid {
+public class Main {
 
     public static void main(String[] args) {
+        GUIDashboard dashboard = new GUIDashboard();
+        dashboard .setVisible(true);
         System.out.println("Hello World!");
     }
 }
